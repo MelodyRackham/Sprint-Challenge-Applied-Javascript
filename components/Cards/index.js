6129d07a0d838d1ebf4204e0bb0df3cdc2d7068d
Mel Maki <melody.rackham@gmail.com>
2019-10-04 09:49:43 -0600

    Finished tabs

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,41 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
+  console.log(response.data);
+  //   const myInfo = response.data;
+  //   const cards = document.querySelector('.cards');
+  //   const newCard = gitCard(myInfo);
+  //   cards.appendChild(newCard);
+});
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function articleCreator(info) {
+  // define new elements
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('ing');
+  const authorName = document.createElement('span');
+
+  // set class names
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  // set text content
+  headline.textContent = info.headline;
+  Image.src = info.authorPhoto;
+  authorName.textContent = info.authorName;
+  // set up structure of elements
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(image);
+  image.appendChild(authorName);
+
+  return card;
+}
